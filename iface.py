b05b71dc60c41dfd0444db840fefb0558dc18e09
Andrew <drewg3r@gmail.com>
2020-03-11 22:27:41 +0200

    Adding names to listWidgets from core.py

--- iface.py
+++ iface.py
@@ -17,3 +17,9 @@ class MyApp(QtWidgets.QMainWindow, interface.w1.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # Adding items to men and women listWidgets
+        for i in core.men_names:
+            self.listWidget_men_names.addItem(i)
+        for i in core.women_names:
+            self.listWidget_women_names.addItem(i)
