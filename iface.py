b76769782f669215eab0d4e0c4ad4abee7abb24d
Andrew <drewg3r@gmail.com>
2020-03-12 19:11:25 +0200

    Added stub functions for buttons pressed, changed pushButton_mtr_rms name

--- iface.py
+++ iface.py
@@ -23,3 +23,79 @@ class MyApp(QtWidgets.QMainWindow, interface.w1.Ui_MainWindow):
             self.listWidget_men_names.addItem(i)
         for i in core.women_names:
             self.listWidget_women_names.addItem(i)
+
+        # Linking buttons to functions
+        self.pushButton_var_clc.clicked.connect(self.var_clc_btn)
+
+        self.pushButton_cpy.clicked.connect(self.cpy_btn)
+
+        self.pushButton_read_a.clicked.connect(self.read_a_btn)
+        self.pushButton_save_a.clicked.connect(self.save_a_btn)
+        self.pushButton_clear_a.clicked.connect(self.clear_a_btn)
+
+        self.pushButton_read_b.clicked.connect(self.read_b_btn)
+        self.pushButton_save_b.clicked.connect(self.save_b_btn)
+        self.pushButton_clear_b.clicked.connect(self.clear_b_btn)
+
+        self.pushButton_mtr_s.clicked.connect(self.mtr_s_btn)
+        self.pushButton_mtr_r.clicked.connect(self.mtr_r_btn)
+        self.pushButton_mtr_rus.clicked.connect(self.mtr_rus_btn)
+        self.pushButton_mtr_rds.clicked.connect(self.mtr_rds_btn)
+        self.pushButton_mtr_rms.clicked.connect(self.mtr_rms_btn)
+        self.pushButton_mtr_umr.clicked.connect(self.mtr_umr_btn)
+        self.pushButton_mtr_st.clicked.connect(self.mtr_st_btn)
+
+        self.pushButton_abt.clicked.connect(self.abt_btn)
+
+    def var_clc_btn(self):
+        try:
+            G = int(self.lineEdit_grp_num.text())
+            N = int(self.lineEdit_ord_num.text()) + 2
+            self.label_9.setText(str((N + G % 60) % 30 + 1))
+        except:
+            self.label_9.setText("??")
+
+    def cpy_btn(self):
+        pass
+
+    def read_a_btn(self):
+        pass
+
+    def save_a_btn(self):
+        pass
+
+    def clear_a_btn(self):
+        pass
+
+    def read_b_btn(self):
+        pass
+
+    def save_b_btn(self):
+        pass
+
+    def clear_b_btn(self):
+        pass
+
+    def mtr_s_btn(self):
+        pass
+
+    def mtr_r_btn(self):
+        pass
+
+    def mtr_rus_btn(self):
+        pass
+
+    def mtr_rds_btn(self):
+        pass
+
+    def mtr_rms_btn(self):
+        pass
+
+    def mtr_umr_btn(self):
+        pass
+
+    def mtr_st_btn(self):
+        pass
+
+    def abt_btn(self):
+        pass
